9c2869b61df3fe8b7f2dd4728278503a5fb1ae41
Sydney Somerfield <sydneysomerfield@gmail.com>
2021-10-21 17:42:03 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const path = require('path')  // Core node module
+const express = require('express') 
+const dotenv = require('dotenv').config()
+const hbs = require('hbs')
+
+const app = express()   // express is a function, so we have to start it up
+const port = process.env.PORT || 31474 
+
+// define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')   // hbs --> npm handlebars module that integrates with express 
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// set up static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+// app.get('', (req, res) => {
+//     res.render('index', {
+//         title: 'Weather',
+//         name: 'Sydney Somerfield'
+//     })
+// })
+
+// start the server listening
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '.')
+})
